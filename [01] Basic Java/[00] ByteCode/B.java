8064aca47e01bb64b7c019f5e8ab63f93f7577bc
nitinmehra09 <nitinmehra9983@gmail.com>
2025-04-06 23:26:45 +0530

    Understanding of Byte code

---
+++ [01] Basic Java/[00] ByteCode/B.java
@@ -0,0 +1,19 @@
+class A{
+    public static void main(String[] args){
+        System.out.println("Hello from Class A");
+
+    }
+}
+public class B{
+    public static void main1(String[] args){
+        System.out.println("Hello from Class B");
+    }
+    public static void main(String[] args){
+        System.out.println("Hello from Class B + main:1");
+    }
+}
+class C{
+    public static void main(String[] args){
+        System.out.println("Hello from Class C");
+    }
+}
