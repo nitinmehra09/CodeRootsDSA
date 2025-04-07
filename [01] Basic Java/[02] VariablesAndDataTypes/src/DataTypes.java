c95ea1121da8dc5640e82e096c8513ca6cee9da5
nitinmehra09 <nitinmehra9983@gmail.com>
2025-04-07 10:39:39 +0530

    DataType is added :)

---
+++ [01] Basic Java/[02] VariablesAndDataTypes/src/DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes{
+    public static void main(String[] args) {
+        // integer 
+        byte a = 10;
+        short b = (short)100;
+        long c = 1000l;
+        int d = 999;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        // float
+        float f1 = (float)5.3;
+        float f2 = 5.3f;
+        double d1 = 5.3;
+        double d2 = 566.2d;
+        System.out.println("f1 = "+f1);
+        System.out.println("f2 = "+f2);
+        System.out.println("d1 = "+d1);
+        System.out.println("d2 = "+d2);
+        //char
+        char c1 = 'a';
+        char c2 = '$';
+        System.out.println("c1 = "+c1);
+        System.out.println("c2 = "+c2);
+
+
+    }
+}
