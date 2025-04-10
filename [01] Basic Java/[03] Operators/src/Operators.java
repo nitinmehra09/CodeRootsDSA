18bc539b5e9af8745188fb657765b2939fc9472a
nitinmehra09 <nitinmehra9983@gmail.com>
2025-04-10 14:52:05 +0530

    Operator is added :)

---
+++ [01] Basic Java/[03] Operators/src/Operators.java
@@ -0,0 +1,39 @@
+public class Operators {
+    public static void main(String[] args) {
+        // unary operators
+        int a = 10;
+        int ans = ++a + a++ + a-- - a++;
+                    // 11 +11  +   12   - 11 = 23
+        System.out.println("ans = " + ans);
+        System.out.println("a = " + a);
+        // a =12
+        
+        //Not operator(!=)
+
+        //(~)
+        int val1 = 11;
+        System.out.println(~val1);
+
+
+        //Arithmatic operator
+        int div = 9/5;
+        System.out.println(div);
+
+        //logical operator
+        if (true && true ){
+            System.out.println("this code is not run");
+        }
+         
+        //Shif Operator
+        int p = 2 ;
+        p = p<<2;
+        System.out.println(p);
+
+        //instanceof {}
+        // to check it is object of class or not 
+
+        //ternary operator
+        boolean f = a>val1?true:false;
+        System.out.println(f);
+    }
+}
