e668314c7fa22ce9c8c22b15a4cc869df0b32888
nitinmehra09 <nitinmehra9983@gmail.com>
2025-06-12 23:57:53 +0530

    Array programs are added

---
+++ [02] Data Structures/Arrays/SimpleArrays/src/MyArray.java
@@ -0,0 +1,9 @@
+public class MyArray {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50,60,70,80,90,100};
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
