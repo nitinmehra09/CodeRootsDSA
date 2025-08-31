be52941d274433497958c360acb33da7e80e27ee
nitinmehra09 <nitinmehra9983@gmail.com>
2025-08-31 14:49:07 +0530

    Two more programs in BS

---
+++ [02] Data Structures/Arrays/BinarySearch/src/floorBinarySearch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class floorBinarySearch {
+    public static void main(String[] args) {
+        int []a = new int[5];
+        try (Scanner sc = new Scanner(System.in)) {
+            for(int i = 0; i<a.length; i++){
+                a[i] = sc.nextInt();
+            }
+        }
+        int index = binarySearch(a,5);
+        if (index != -1)
+            System.out.println("Element's first Occurrence : " + index);
+        else
+            System.out.println("Element not found.");
+    }
+    public static int binarySearch(int a[], int x){
+            int L = 0 , H = a.length-1;
+            int result = -1;
+            while(L<=H){
+                int mid = L + (H - L) / 2;
+                if(a[mid]==x){
+                    result=mid;
+                    H = mid -1;
+                }
+                else if (a[mid]<x)
+                    L = mid +1;
+                else if (a[mid]>x)
+                    H = mid -1;
+            }
+            return result;
+        }
+}
