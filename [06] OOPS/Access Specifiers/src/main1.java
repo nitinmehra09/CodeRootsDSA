bebe255c5b271b351a52db4e43ce4a2fffae087a
nitinmehra09 <nitinmehra9983@gmail.com>
2025-11-30 13:44:10 +0530

    Access Specifiers is added

---
+++ [06] OOPS/Access Specifiers/src/main1.java
@@ -0,0 +1,20 @@
+class acs{
+     private int x;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
+public class main1 {
+    public static void main(String[] args) {
+        acs a = new acs();
+        // int x = a.x = 20;
+        a.setX(20);
+        int x = a.getX();
+        System.out.println(x);
+    }
+}
