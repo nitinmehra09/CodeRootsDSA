de85cd5dc2a97a2e2ddc52a68a7df06445e37f18
nitinmehra09 <nitinmehra9983@gmail.com>
2025-12-02 23:41:33 +0530

    leetcode solution is added

---
+++ [05] LeetcodeSol/Easy/Stack/20. Valid Parentheses/src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if((s.length()%2)!=0){
+            return false;
+        }
+        for (int i =0; i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch =='[' || ch =='{'){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else{
+                    char top = stack.peek();
+                    if(ch=='}' && top!='{'){
+                        return false;
+                    }
+                    else if(ch==']' && top!='[' ){
+                        return false;
+                    }
+                    else if(ch==')' && top!='(' ){
+                        return false;
+                    }
+                    else{
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
